e43e27e8723fa29eed55aa5152775e81e45a2106
Elvis Vermilyea <9233802@isd535.org>
2021-09-14 09:42:10 -0500

    bol

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+let dx = 2;
+let dy = -2;
 
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
@@ -7,11 +12,19 @@ ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.strokeStyle = "#00FF00";
-ctx.fill();
-ctx.closePath();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.strokeStyle = "#00FF00";
+    ctx.fill();
+    ctx.closePath();
+
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
 
 ctx.beginPath();
 ctx.rect(160, 10, 100, 40);

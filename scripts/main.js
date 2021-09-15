13adbfd55a3a74b06a8b32a3706a1ee8871529fc
Elvis Vermilyea <9233802@isd535.org>
2021-09-15 08:59:44 -0500

    bol bonce uf wol

--- scripts/main.js
+++ scripts/main.js
@@ -6,23 +6,33 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
+let ballRadius = 10;
+
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.strokeStyle = "#00FF00";
     ctx.fill();
     ctx.closePath();
+}
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     x += dx;
     y += dy;
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 }
 setInterval(draw, 10);
 
